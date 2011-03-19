cd4da1acbc546652bce25958ca3619f503a668aa
Kamil Kaczmarczyk <kamilkaczm@gmail.com>
2011-03-19 18:26:31 +0100

    some fixes for imap

--- src/com/fsck/k9/criteriafilter/CriteriaFilter.java
+++ src/com/fsck/k9/criteriafilter/CriteriaFilter.java
@@ -1,6 +1,6 @@
 package com.fsck.k9.criteriafilter;
 
-import com.fsck.k9.mail.Flag;
+import java.util.ArrayList;
 import com.fsck.k9.mail.Message;
 import com.fsck.k9.mail.MessagingException;
 import com.fsck.k9.controller.MessagingController;
@@ -13,19 +13,35 @@ public class CriteriaFilter {
     /**
      * Apply the filter to a message, performing appropriate actions if
      * necessary.
-     *
-     * @param controller Controller to deal with the message
+     * 
      * @param message Local message to be checked
      *
-     * @return true if the new message should appear in a folder, otherwise false
+     * @return true if the new message should appear in the folder view.
+     *         false means there's no point displaying or downloading the message
      * @throws MessagingException from Message.setFlag()
      */
-    public boolean ApplyToMessage(final MessagingController controller, Message message) throws MessagingException {
+    public boolean ApplyToMessage(Message message) throws MessagingException {
+    	if (mMessagesToDelete.contains(message)) {
+    		//already's been filtered
+    		return false;
+    	}
+    	
         if (message.getSubject() != null && message.getSubject().contains("spam")) {
-		message.setFlag(Flag.SEEN, true); //mark as seen to avoid notifications
-		controller.deleteMessages(new Message[]{message}, null);//delete
-		return false;
+	        mMessagesToDelete.add(message);
+	        return false;
         }
         return true;
     }
+    
+    /**
+     * Performs actions according to the results of the filtering. 
+     * @param controller Controller to deal with the message
+     */
+    public void PerformActions(final MessagingController controller) {
+    	for (Message message : mMessagesToDelete) {
+    		controller.deleteMessages(new Message[]{message}, null); 
+    	}
+    }
+    
+    ArrayList<Message> mMessagesToDelete = new ArrayList<Message>(); 
 }
