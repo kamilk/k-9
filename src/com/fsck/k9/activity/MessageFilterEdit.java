3d5a793ac7715883a55a1df150fd2bc2ad52ff5f
Kamil Kaczmarczyk <kamilkaczm@gmail.com>
2011-03-25 19:07:16 +0100

    some message filter settings screen improvements

--- src/com/fsck/k9/activity/MessageFilterEdit.java
+++ src/com/fsck/k9/activity/MessageFilterEdit.java
@@ -1,11 +1,18 @@
 package com.fsck.k9.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.FrameLayout;
 
 import com.fsck.k9.R;
 import com.fsck.k9.activity.K9Activity;
 
+/**
+ * An activity for editing a message filter
+ */
 public class MessageFilterEdit extends K9Activity {
 	private LinearLayout mCriterionList;
 
@@ -13,9 +20,26 @@ public class MessageFilterEdit extends K9Activity {
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 
+		//fill the content
 		setContentView(R.layout.message_filter_edit);
-
 		mCriterionList = (LinearLayout)findViewById(R.id.criterion_list);
-		getLayoutInflater().inflate(R.layout.message_filter_criterion, mCriterionList);
+
+		Button addButton = (Button)findViewById(R.id.add_new_criterion);
+		addButton.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v) {
+				addNewCriterionField();
+			}
+		});
+
+		addNewCriterionField();
+	}
+
+	public void addNewCriterionField() {
+		FrameLayout criterion_frame = new FrameLayout(this);
+		criterion_frame.setClipChildren(false);
+		getLayoutInflater().inflate(R.layout.message_filter_criterion, criterion_frame);
+		mCriterionList.addView(criterion_frame);
 	}
 }
